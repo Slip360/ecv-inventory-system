63cad96cb98cd5b16c5669c4d791884c85186c54
José Ramírez <josecatun2015@hotmail.com>
2026-07-03 00:37:57 -0600

    feat: create inventory-update-modal component scaffolding

---
+++ src/app/pages/inventory-page/components/inventory-update-modal/inventory-update-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-update-modal',
+  imports: [],
+  templateUrl: './inventory-update-modal.html',
+  styleUrl: './inventory-update-modal.css',
+})
+export class InventoryUpdateModal {
+
+}
